9022f482defed222eb47095c744cd906a9b95c01
GlaringMarvel <104287971+GlaringMarvel@users.noreply.github.com>
2023-08-07 18:00:36 +0800

    Update

--- HangarMenu.py
+++ HangarMenu.py
@@ -1,23 +1,23 @@
 import time
-
 from python_imagesearch.imagesearch import imagesearcharea  # 引入识图函数
-def CooData(x,y):
+
+def cooData(x,y):
     counter = 1   # 循环计数器
     while counter <= 5:
         Num = 0   # 获取图片个数
-        pos = imagesearcharea("image/HangarMenu/Setting.png", x, y, x+192, y+128)
+        pos = imagesearcharea("image/HangarMenu/Setting.png", x, y, x+192, y+128, precision=0.5)
         if pos[0] != -1:
             Num += 1
         else:
             print("'Setting' image not found")
 
-        pos = imagesearcharea("image/HangarMenu/Battle.png", x+400, y, x+800, y+300)
+        pos = imagesearcharea("image/HangarMenu/Battle.png", x+400, y, x+800, y+300, precision=0.5)
         if pos[0] != -1:
             Num += 1
         else:
             print("'Battle' image not found")
 
-        pos = imagesearcharea("image/HangarMenu/Help.png", x + 1000, y, x + 1500, y + 128)
+        pos = imagesearcharea("image/HangarMenu/Help.png", x + 1000, y, x + 1500, y + 128, precision=0.5)
         if pos[0] != -1:
             Num += 1
         else:
@@ -25,11 +25,10 @@ def CooData(x,y):
 
         if Num == 3:
             return 1  # 返回值1，识别成功
-            break
         else:
             print("图像识别失败，请查看游戏界面是否被遮挡")
-            print("第"+ str(counter)+"次识别失败")
+            print("第" + str(counter)+"次识别失败")
             counter += 1
             time.sleep(3)
-    print("界面识别失败，请检查")
+    print("界面识别失败，请检查界面或等待")
     return 0  # 返回值0，识别失败
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
+import time                 # 调用时间函数
 import GetWindow            # 调用窗口索引
 import HangarMenu as hm     # 调用机库识图
+import pyautogui            # 调用鼠标移动
+import keyboard             # 调用键盘按键
 
 
 x,y=GetWindow.returData()   # 调用模块获得窗口坐标
 
-# 将窗口坐标传入机库识图模块
-def pushDatatohm(x,y):
-    hm.CooData(x,y)
+def Click():
+    pyautogui.click()
+    time.sleep(0.1)
+    pyautogui.mouseUp()
 
-# 获得机库识图模块返回值，值为1则成功，值为0则失败
-Bollen = hm.CooData(x,y)
-print(Bollen)
+def Esc():
+    keyboard.press('esc')
+    time.sleep(0.1)
+    keyboard.release('esc')
+
+def starGame(x,y):
+    pyautogui.moveTo(x + 650, y + 70)
+    time.sleep(1)
+    Click()
+
+# 机库界面循环
+while True:
+# 将窗口坐标传入机库识图模块,获得机库识图模块返回值，值为1则成功，值为0则失败
+    Bollen = hm.cooData(x,y)
+    if Bollen == 0:
+        starGame(x,y)
+        time.sleep(1)
+        Esc()
+        time.sleep(1)
+    else:
+        print("图像识别成功")
+        break
+
+starGame(x,y)
+time.sleep(1)
